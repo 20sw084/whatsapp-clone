ae04ab3a5e7c63068c3fd7312e338d9ef963dc89
Jonny <junaidaslam.muet@gmail.com>
2023-08-04 23:12:42 +0500

    2nd commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -7,80 +7,83 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Container(
-        height: 50,
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Message",
-                    prefixIcon: IconButton(
-                      color: Colors.grey,
-                      icon: Icon(Icons.emoji_emotions_outlined),
-                      onPressed: () {  },
-                    ),
-                    suffixIcon: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          color: Colors.grey,
-                          icon: Icon(Icons.attachment),
-                          onPressed: () {  },
-                        ),
-                        IconButton(
-                          color: Colors.grey,
-                          icon: Icon(Icons.camera_alt),
-                          onPressed: () {  },
-                        ),
-                      ],
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          height: 50,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+
+                    decoration: InputDecoration(
+                      hintText: "Message",
+                      prefixIcon: IconButton(
+                        color: Colors.grey,
+                        icon: Icon(Icons.emoji_emotions_outlined),
+                        onPressed: () {  },
+                      ),
+                      suffixIcon: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            color: Colors.grey,
+                            icon: Icon(Icons.attachment),
+                            onPressed: () {  },
+                          ),
+                          IconButton(
+                            color: Colors.grey,
+                            icon: Icon(Icons.camera_alt),
+                            onPressed: () {  },
+                          ),
+                        ],
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              FloatingActionButton(onPressed: null, child: Icon(Icons.mic_rounded)),
-            ],
+                FloatingActionButton(onPressed: null, child: Icon(Icons.mic_rounded)),
+              ],
+            ),
           ),
         ),
-      ),
-      appBar: AppBar(
-        title: Text(title),
-        leading: Stack(
-          children: [
-            IconButton(
-              iconSize: 25,
-              onPressed: () => Navigator.of(context).pop(),
-              icon: Icon(
-                Icons.arrow_back,
+        appBar: AppBar(
+          title: Text(title),
+          leading: Stack(
+            children: [
+              IconButton(
+                iconSize: 25,
+                onPressed: () => Navigator.of(context).pop(),
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
               ),
+              CircleAvatar(
+                radius: 20,
+                child: Image.network(imgURL),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.videocam),
             ),
-            CircleAvatar(
-              radius: 20,
-              child: Image.network(imgURL),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.call),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.more_vert),
             ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.videocam),
-          ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.call),
-          ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.more_vert),
-          ),
-        ],
       ),
     );
   }
